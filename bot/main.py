ee9e987ed9c03e635a923107ab0747136cb0fdd3
Jethro Yu <jethro@teamt5.org>
2023-05-02 10:43:43 +0800

    feat: loading prompt from assistant_prompt.txt

--- bot/main.py
+++ bot/main.py
@@ -27,6 +27,8 @@ def main():
     # Setup configurations
     model = os.environ.get('OPENAI_MODEL', 'gpt-3.5-turbo')
     max_tokens_default = default_max_tokens(model=model)
+    with open('assistant_prompt.txt', 'r') as f:
+        ASSISTANT_PROMPT = f.read()
     openai_config = {
         'api_key': os.environ['OPENAI_API_KEY'],
         'show_usage': os.environ.get('SHOW_USAGE', 'false').lower() == 'true',
@@ -34,7 +36,7 @@ def main():
         'proxy': os.environ.get('PROXY', None),
         'max_history_size': int(os.environ.get('MAX_HISTORY_SIZE', 15)),
         'max_conversation_age_minutes': int(os.environ.get('MAX_CONVERSATION_AGE_MINUTES', 180)),
-        'assistant_prompt': os.environ.get('ASSISTANT_PROMPT', 'You are a helpful assistant.'),
+        'assistant_prompt': os.environ.get('ASSISTANT_PROMPT', ASSISTANT_PROMPT),
         'max_tokens': int(os.environ.get('MAX_TOKENS', max_tokens_default)),
         'n_choices': int(os.environ.get('N_CHOICES', 1)),
         'temperature': float(os.environ.get('TEMPERATURE', 1.0)),
